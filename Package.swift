f24ab9de69c099ff76e9a503cc671e4a90cf5070
Pavel Yevtukhov <pavlo.yevtukhov@globallogic.com>
2024-08-29 16:39:52 +0300

    experimental release

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+private let artifactVersion = "0.0.1"
+
+let package = Package(
+    name: "ada-local-xcframework",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "AdaSdk",
+            targets: ["AdaSdkTarget"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")
+    ],
+    targets: [
+        .target(
+            name: "AdaSdkTarget",
+            dependencies: [
+                .target(name: "AdaSdk"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ]
+        ),
+        .binaryTarget(
+            name: "AdaSdk",
+            url: "https://github.com/Aniview/ada-sdk-ios-spm/releases/download/artifact_\(artifactVersion)/AdaSdk.zip",
+            checksum: "5397977d32b4f07397ab2a9c26a630ae25e80e572c830d8db8ddf709673f76fe"
+        )
+    ]
+)
